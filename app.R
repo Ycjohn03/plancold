4655c582c05fc534815002d64fc2706c2dfd58f4
Ycjohn03 <49867555+Ycjohn03@users.noreply.github.com>
2019-04-23 09:31:51 +0800

    Add files via upload

--- app.R
+++ app.R
@@ -1,5 +1,5 @@
 nnamme <- readRDS("namme.rds")
-data.t_hour <- readRDS("data.t_hour.rds")[-c(700:702),]
+data.t_hour <- readRDS("data.t_hour.rds")
 names(data.t_hour) <- (nnamme$V1)
 
 library(shinythemes)
